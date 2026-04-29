23144a8ca3c66ffe3cc361d82238b36d0813e1eb
naga2588 <willy.zeroun90@gmail.com>
2026-04-29 14:53:04 +0700

    fix login id

--- app/src/main/java/com/example/kendaraanapp/LoginActivity.kt
+++ app/src/main/java/com/example/kendaraanapp/LoginActivity.kt
@@ -11,13 +11,13 @@ class LoginActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val user = findViewById<EditText>(R.id.etUser)
-        val pass = findViewById<EditText>(R.id.etPass)
+        val user = findViewById<EditText>(R.id.etUsername)
+        val pass = findViewById<EditText>(R.id.etPassword)
         val btn = findViewById<Button>(R.id.btnLogin)
 
         btn.setOnClickListener {
 
-            if (user.text.toString() == "Admin" && pass.text.toString() == "Naga2588") {
+            if (user.text.toString() == "Admin" && pass.text.toString() == "1234") {
                 startActivity(Intent(this, MainActivity::class.java))
             } else {
                 Toast.makeText(this, "Login gagal", Toast.LENGTH_SHORT).show()

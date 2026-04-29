6061202020a5fcf4fe788150b78f548080c06e72
naga2588 <willy.zeroun90@gmail.com>
2026-04-29 12:15:07 +0700

    fix: remove nested git

---
+++ app/src/main/java/com/app/LoginActivity.kt
@@ -0,0 +1,27 @@
+package com.app
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.widget.*
+
+class LoginActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val user = findViewById<EditText>(R.id.etUser)
+        val pass = findViewById<EditText>(R.id.etPass)
+        val btn = findViewById<Button>(R.id.btnLogin)
+
+        btn.setOnClickListener {
+
+            if (user.text.toString() == "Admin" && pass.text.toString() == "Naga2588") {
+                startActivity(Intent(this, MainActivity::class.java))
+            } else {
+                Toast.makeText(this, "Login gagal", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/app/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.app
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.*
+import org.json.JSONArray
+import java.net.URL
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val input = findViewById<EditText>(R.id.etCari)
+        val btn = findViewById<Button>(R.id.btnCari)
+        val tv = findViewById<TextView>(R.id.tvResult)
+
+        btn.setOnClickListener {
+
+            Thread {
+                val url = "http://192.168.1.10:8000/index.php?key=MY_SECRET_KEY&q=${input.text}"
+                val data = URL(url).readText()
+
+                runOnUiThread {
+                    tv.text = formatJson(data)
+                }
+            }.start()
+        }
+    }
+
+    fun formatJson(json: String): String {
+        val obj = JSONArray(json).getJSONObject(0)
+
+        return """
+DATA KENDARAAN
+════════════════
+Nama : ${obj.getString("NamaPemilik")}
+NIK  : ${obj.getString("NoKTP")}
+Nopol: ${obj.getString("nopol")}
+Merk : ${obj.getString("Merk")}
+"""
+    }
+}
